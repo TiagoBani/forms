90cf966ca093f2bda62069d8da46843963d70aa7
Tiago Bani <tiago.bani@yahoo.com.br>
2018-09-04 21:52:10 -0300

    Commit inicial

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// import { TemplateFormComponent } from './template-form/template-form.component';
+import { DataFormComponent } from './data-form/data-form.component';
+import { TemplateFormModule } from './template-form/template-form.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DataFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    TooltipModule.forRoot(),
+    TemplateFormModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
